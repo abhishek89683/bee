cdc69c167158db8245bedeb902c1018c3b642341
abhishek garg <abhishekgarg@abhisheks-MacBook-Air-2.local>
2025-08-08 18:16:27 +0530

    first commit

---
+++ mangoshek/math.js
@@ -0,0 +1,19 @@
+function add(a,b){
+    return a+b;
+}
+module.exports = {add};
+
+function sub(a,b){
+    return a-b;
+}
+module.exports = {sub};
+
+function mul(a,b){
+    return a*b;
+}
+module.exports = {mul};2
+
+function div(a,b){
+    return a/b;
+}
+module.exports = {div};
---
+++ node.js/server.js
@@ -0,0 +1,33 @@
+
+
+
+// process.on('uncaughtException' , (err) => {
+//     console.error("caught globally: ", err.message);
+//     process.exit(1);
+// });
+
+// setTimeout(() => {
+//     throw new Error("This is an uncught Exception");
+
+// }, 2000);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// what is the differrence between a caught and uncaught exception?
+// why should we wxit the process after an uncaught exception?
+// can you use try... catch to handle errors inside setTimeout? why or why not?
+// what tool can you use in production to restart apps that crashes due to uncaught-exception?
